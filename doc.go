c7b8034af6297c98837fea0c61d61a1a3a71a221
Aaron Schlesinger <arschles@gmail.com>
2015-09-10 11:37:21 -0700

    package docs

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package tme provides interfaces for time.Tickers and time.Timers, and implementations for production and testing
+// for both. Use the interfaces in your code and pass the production & testing implementations as needed.
+package tme
