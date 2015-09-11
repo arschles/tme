4ea50db89ec4cc34de16c6939aa4dd09d035c2f1
Aaron Schlesinger <arschles@gmail.com>
2015-09-11 16:23:09 -0700

    improved docs

--- ticker.go
+++ ticker.go
@@ -3,9 +3,10 @@ package tme
 // Ticker continuously sends on Chan() channel until Stop is called
 type Ticker interface {
 	// Chan returns a channel that receives every time the ticker ticks. The chan will be
-	// closed when ticking is done (e.g. when Stop() is called).
+	// closed when Stop is called
 	//
-	// The receiver of the tick should call ack.Fn() to acknowledge its receipt of the tick.
+	// The receiver of the tick may call ack.Fn() to acknowledge its receipt of the tick,
+	// but it's not a requirement.
 	Chan() <-chan Ack
 	// Stop stops the ticker so that it doesn't send on Chan() anymore. Multiple calls to Stop
 	// are allowed. Returns true if the ticker was running beforehand, false otherwise.
