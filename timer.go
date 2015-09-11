bf23907288ee65e57bf2d217d3269baf525c2c3e
Aaron Schlesinger <arschles@gmail.com>
2015-09-11 15:44:48 -0700

    docs

--- timer.go
+++ timer.go
@@ -2,8 +2,8 @@ package tme
 
 // Timer sends once on the Done() channel unless Stop() is called beforehand
 type Timer interface {
-	// Done returns a channel that receives the current time and closes after the timer is done. It will
-	// neither receive nor be closed if Stop() is closed.
+	// Done returns a channel that receives and closes after the timer is done. All channels returned
+	// by this func will not receive or close if Stop is called
 	Done() <-chan Ack
 	// Stop cancels the Timer. If this timer is not done when Stop is called, the timer
 	// will not send on any channels returned by Done (but it won't close any of those chans).
