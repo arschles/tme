75d7db782984d8ff8b5f50d9b2379b9bb588aa01
Aaron Schlesinger <arschles@gmail.com>
2015-09-18 10:43:55 -0700

    more docs

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
 # tme
 
+[![GoDoc](https://godoc.org/github.com/arschles/tme?status.svg)](https://godoc.org/github.com/arschles/tme)
+
 `tme` is a package that provides unified interfaces around tickers and timers for Go
 programs. It provides a `Timer` and `Ticker` interface, each with an implementation
 that uses real time and an implementation that can be manually controlled.
 
-# Usage
-
-Generally speaking, the manual implementations are used for testing purposes and the real
-implementations for production purposes. Code should operate on the interfaces so that you can
-later implement and swap new implementations in for various different purposes
-
-# Ack
-
-Each of the `Timer` and `Ticker` implementations contained herein use an `Ack` struct to notify
-of events (e.g. the timer finished or a tick happened).
-
-`Ack` contains both the time that the event occurred and a `func()` that you can call
-to acknowledge (ack) the receipt of the event. The creator of the `Ticker`/`Timer` implements
-the `func()`.
+See [the Godoc](https://godoc.org/github.com/arschles/tme) for this package for more documentation.
--- doc.go
+++ doc.go
@@ -1,3 +1,19 @@
-// Package tme provides interfaces for time.Tickers and time.Timers, and implementations for production and testing
-// for both. Use the interfaces in your code and pass the production & testing implementations as needed.
+// `tme` is a package that provides unified interfaces around tickers and timers for Go
+// programs. It provides a `Timer` and `Ticker` interface, each with an implementation
+// that uses real time and an implementation that can be manually controlled.
+//
+// Usage
+//
+// Generally speaking, the manual implementations are used for testing purposes and the real
+// implementations for production purposes. Code should operate on the interfaces so that you can
+// later implement and swap new implementations in for various different purposes
+//
+// Ack
+//
+// Each of the Timer and Ticker implementations contained herein use an Ack struct to notify
+// of events (e.g. the timer finished or a tick happened).
+//
+// Ack contains both the time that the event occurred and a func() that you, the receiver of events can call
+// to acknowledge (ack) the receipt of the event. The creator of the Ticker / Timer implements
+// the func().
 package tme
